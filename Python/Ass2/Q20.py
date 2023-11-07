13c1ee55c06f814176f48b5522167b96f74d21ce
Maheshwar04 <116495825+maheshwar04@users.noreply.github.com>
2023-11-07 07:12:51 +0530

    updated

--- Python/Ass2/Q20.py
+++ Python/Ass2/Q20.py
@@ -1,18 +1,37 @@
 # Define the discount percentage
 discount_percentage = 0.60
 
-# Define a list of original prices
-original_prices = [4.95, 9.95, 14.95, 19.95, 24.95]
-
 # Print the table header
 print("Original Price  Discount  New Price")
 print("----------------------------------")
 
-# Generate and print the table
-for price in original_prices:
-    discount = price * discount_percentage
-    new_price = price - discount
-    print(f"${price:.2f}        ${discount:.2f}    ${new_price:.2f}")
+# Define original prices individually
+price1 = 4.95
+price2 = 9.95
+price3 = 14.95
+price4 = 19.95
+price5 = 24.95
+
+# Calculate and print the table for each price
+discount1 = price1 * discount_percentage
+new_price1 = price1 - discount1
+print(f"${price1:.2f}        ${discount1:.2f}    ${new_price1:.2f}")
+
+discount2 = price2 * discount_percentage
+new_price2 = price2 - discount2
+print(f"${price2:.2f}        ${discount2:.2f}    ${new_price2:.2f}")
+
+discount3 = price3 * discount_percentage
+new_price3 = price3 - discount3
+print(f"${price3:.2f}        ${discount3:.2f}    ${new_price3:.2f}")
+
+discount4 = price4 * discount_percentage
+new_price4 = price4 - discount4
+print(f"${price4:.2f}        ${discount4:.2f}    ${new_price4:.2f}")
+
+discount5 = price5 * discount_percentage
+new_price5 = price5 - discount5
+print(f"${price5:.2f}        ${discount5:.2f}    ${new_price5:.2f}")
 
 #op
 #Original Price  Discount  New Price
