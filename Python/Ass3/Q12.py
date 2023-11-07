e45649f6fb2fb37181a91752acc3cd877a8362f8
Maheshwar04 <116495825+maheshwar04@users.noreply.github.com>
2023-11-07 13:50:12 +0530

    added

--- Python/Ass3/Q12.py
+++ Python/Ass3/Q12.py
@@ -1,4 +1,4 @@
-#1
+# 1
 # def print_pattern(rows):
 #     for i in range(1, rows + 1):
 #         for j in range(1, i + 1):
@@ -9,53 +9,65 @@
 
 #2
 #3
-#def print_pattern(rows):
+# def print_pattern(rows):
 #    for i in range(rows,0,-1):
 #        for j in range(i,0,-1):
 #            print(j,end=" ")
 #        print()
-#num_rows=5
-#print_pattern(num_rows)
+# num_rows=5
+# print_pattern(num_rows)
 
 #4
-#def print_pattern(rows):
+# def print_pattern(rows):
 #    for i in range(1,rows+1):
 #        for j in range(0,i):
 #            print(i,end=" ")
 #        print()
-#num_rows=5
-#print_pattern(num_rows)
+# num_rows=5
+# print_pattern(num_rows)
 
 #5
 #def print_pattern(rows):
 #    for i in range(rows):
 #       for j in range(i):
 #            print( " ",end=" ")
-#        for k in range(rows-i):
+#       for k in range(rows-i):
 #            print(k+i+1,end=" ")
-#        print()    
+#       print()    
     
-#num_rows=5
-#print_pattern(num_rows)
+# num_rows=5
+# print_pattern(num_rows)
 
 #7
-#def print_pattern(rows):
-#    for i in range(rows):
+# def print_pattern(rows):
+#     for i in range(rows):
 #        for j in range(rows):
 #           print("*",end=" ")
 #        print()    
     
-#num_rows=5
-#print_pattern(num_rows)
+# num_rows=5
+# print_pattern(num_rows)
 
 #Q13
+# def print_pattern(rows):
+#    for i in range(rows):
+#        for j in range(i):
+#            print( " ",end=" ")
+#        for k in range(rows-i):
+#            print("$",end=" ")
+#        print()    
+    
+# num_rows=5
+# print_pattern(num_rows)
+
+#Q14
 def print_pattern(rows):
-   for i in range(rows):
-       for j in range(i):
-           print( " ",end=" ")
+   for i in range(rows+1):
        for k in range(rows-i):
-           print("$",end=" ")
-       print()    
+           print(" ",end=" ")   
+       for j in range(i):
+           print( "#",end=" ")
+       print()
     
 num_rows=5
 print_pattern(num_rows)
